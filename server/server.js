860159a986356d95120008a3873da9d53d1062d3
Raz Haimovich <43092616+RazHaimovich@users.noreply.github.com>
2022-01-21 09:22:07 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const http = require('http');
 const server = http.createServer(app);
 const socketIo = require("socket.io");
 const usersManager = require('./usersManager');
-const elasticsearch = require('elasticsearch');
 
 require('dotenv').config();
 
